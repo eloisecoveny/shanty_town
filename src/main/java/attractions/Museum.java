fccc283ea7673b414543c94c55dfbb9a9995818b
Eloise Coveny <eloisecoveny@gmail.com>
2019-03-27 08:32:09 +0000

    updated enter dialogue in museum

--- src/main/java/attractions/Museum.java
+++ src/main/java/attractions/Museum.java
@@ -20,7 +20,7 @@ public class Museum extends Attraction implements ITicket, IEnter {
 
     public String enter(Visitor visitor){
         this.charge(visitor);
-        return "As your close the door behind you the wooden floorboards beneath foot creak and cradle your weight. Dust particles float around you as you breathe in the cloggy scent of old industry and horse-wear. That will cost $10 please, askss the woman behind the counter. Small round spectacles and some unknown scar around the circumference of her neck. Please remember to leave a comment on your way out.";
+        return "As your close the door behind you the wooden floorboards beneath foot creak and cradle your weight. Dust particles float around you as you breathe in the cloggy scent of old industry and horse-wear. That will cost $10 please, asks the woman behind the counter. Small round spectacles and some unknown scar around the circumference of her neck. Please remember to leave a review on your way out.";
     }
 
     public void isIssuedReview(Review review){
